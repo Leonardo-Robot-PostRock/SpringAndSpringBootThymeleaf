350d4aa3ad8b6c08fef2b8b9dd0b1afa1a533262
Leonardo <leonardo38361@gmail.com>
2024-02-12 02:15:26 -0300

    creación de una lista

--- src/main/java/com/springbloom/SpringBloom/StartupController.java
+++ src/main/java/com/springbloom/SpringBloom/StartupController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Controller
 @Slf4j
 public class StartupController {
@@ -22,11 +25,26 @@ public class StartupController {
         person.setEmail("leonardo@gmail.com");
         person.setPhone("234123123");
 
+        var person2 = new Person();
+
+        person2.setName("Ada");
+        person2.setSurname("Crain");
+        person2.setEmail("ada@gmail.com");
+        person2.setPhone("2341245432");
+
         log.info("Ejecutando el controlador Spring MVC");
+
+//        var persons = new ArrayList<Person>();
+//        persons.add(person);
+
+        var persons = Arrays.asList(person, person2);
+
+//        var persons = new ArrayList<>();
 
         model.addAttribute("message", message);
         model.addAttribute("greeting", greeting);
-        model.addAttribute("person", person);
+//        model.addAttribute("person", person);
+        model.addAttribute("persons", persons);
 
 //        log.debug("más detalle del controlador");
         return "index";
