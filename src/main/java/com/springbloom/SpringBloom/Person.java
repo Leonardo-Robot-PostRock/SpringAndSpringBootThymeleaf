8a68546ef5c5e82038363829ebcc9db5600e8d01
Leonardo <leonardo38361@gmail.com>
2024-02-12 01:27:15 -0300

    creación de clase person

---
+++ src/main/java/com/springbloom/SpringBloom/Person.java
@@ -0,0 +1,11 @@
+package com.springbloom.SpringBloom;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private  String name;
+    private  String surname;
+    private  String email;
+    private  String phone;
+}
