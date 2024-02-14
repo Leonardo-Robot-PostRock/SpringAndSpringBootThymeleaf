645e9a65b78595600849a3c3b4c8ae2e6c910eec
Leonardo <leonardo38361@gmail.com>
2024-02-14 04:37:16 -0300

    archivo renombrado y cambio de ruta. Se crea el pojo Person y se eliminan pruebas en controlador

--- src/main/java/com/springbloom/SpringBloom/domain/Person.java
+++ src/main/java/com/springbloom/SpringBloom/domain/Person.java
@@ -1,9 +1,19 @@
-package com.springbloom.SpringBloom;
+package com.springbloom.SpringBloom.domain;
 
+import jakarta.persistence.*;
+import java.io.Serializable;
 import lombok.Data;
 
-@Data
-public class Person {
+@Data//Acceso a todos los metodos get, set y toString
+@Entity//Pojo
+@Table(name = "person")//solución a letra mayúscula de Person to person of the database.
+public class Person implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY)
+    private Long idPerson;
     private  String name;
     private  String surname;
     private  String email;
--- src/main/java/com/springbloom/SpringBloom/web/StartupController.java
+++ src/main/java/com/springbloom/SpringBloom/web/StartupController.java
@@ -1,4 +1,4 @@
-package com.springbloom.SpringBloom;
+package com.springbloom.SpringBloom.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,41 +12,13 @@ import java.util.Arrays;
 @Controller
 @Slf4j
 public class StartupController {
-    @Value("${index.greeting}")
-    private String greeting;
 
     @GetMapping("/")
     public String start(Model model){
-        var message = "Message with Thymeleaf";
-        var person = new Person();
-
-        person.setName("Leonardo");
-        person.setSurname("Puebla");
-        person.setEmail("leonardo@gmail.com");
-        person.setPhone("234123123");
-
-        var person2 = new Person();
-
-        person2.setName("Ada");
-        person2.setSurname("Crain");
-        person2.setEmail("ada@gmail.com");
-        person2.setPhone("2341245432");
-
         log.info("Ejecutando el controlador Spring MVC");
 
-//        var persons = new ArrayList<Person>();
-//        persons.add(person);
+        //model.addAttribute("persons", persons);persons
 
-        var persons = Arrays.asList(person, person2);
-
-//        var persons = new ArrayList<>();
-
-        model.addAttribute("message", message);
-        model.addAttribute("greeting", greeting);
-//        model.addAttribute("person", person);
-        model.addAttribute("persons", persons);
-
-//        log.debug("más detalle del controlador");
         return "index";
     }
 }
