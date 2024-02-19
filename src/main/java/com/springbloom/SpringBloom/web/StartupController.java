3730462c1cfdf831bb6ff60957026bb92ac8f209
Leonardo <leonardo38361@gmail.com>
2024-02-18 23:49:05 -0300

    Conversión de clase a entidad

--- src/main/java/com/springbloom/SpringBloom/web/StartupController.java
+++ src/main/java/com/springbloom/SpringBloom/web/StartupController.java
@@ -1,5 +1,6 @@
 package com.springbloom.SpringBloom.web;
 
+import com.springbloom.SpringBloom.dao.PersonDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,16 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 @Slf4j
 public class StartupController {
-
+    
+    @Autowired
+    private PersonDao personDao;
+    
     @GetMapping("/")
     public String start(Model model){
+        var persons = personDao.findAll();
+        
         log.info("Ejecutando el controlador Spring MVC");
 
-        //model.addAttribute("persons", persons);persons
+        model.addAttribute("persons", persons);
 
         return "index";
     }
