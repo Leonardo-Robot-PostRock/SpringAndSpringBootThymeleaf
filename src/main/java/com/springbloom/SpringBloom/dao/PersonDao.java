6ffa72951cb4b7180b7b18605c55db836e3a66b7
Leonardo <leonardo38361@gmail.com>
2024-03-05 20:08:07 -0300

    se extiende de jparepository

--- src/main/java/com/springbloom/SpringBloom/dao/PersonDao.java
+++ src/main/java/com/springbloom/SpringBloom/dao/PersonDao.java
@@ -1,17 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.springbloom.SpringBloom.dao;
 
 import com.springbloom.SpringBloom.domain.Person;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author mrRobot
  */
 
-public interface PersonDao extends CrudRepository<Person, Long>{
+public interface PersonDao extends JpaRepository<Person, Long>{
     
 }
