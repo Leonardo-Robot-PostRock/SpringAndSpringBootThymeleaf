a530dc1cf216f5259e0bbeb437ad4406db43d027
Leonardo <leonardo38361@gmail.com>
2024-03-05 16:50:31 -0300

    visualizar usuario logeado

--- src/main/java/com/springbloom/SpringBloom/web/StartupController.java
+++ src/main/java/com/springbloom/SpringBloom/web/StartupController.java
@@ -4,6 +4,8 @@ import com.springbloom.SpringBloom.domain.Person;
 import com.springbloom.SpringBloom.service.ServicePerson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,21 +22,21 @@ public class StartupController {
     private ServicePerson servicePerson;//Spring busca una clase que implemente la interface ServicePerson. 
 
     @GetMapping("/")
-    public String start(Model model) {
+    public String start(Model model, @AuthenticationPrincipal User user) {
         var persons = servicePerson.listPersons();
-
+        
         log.info("Ejecutando el controlador Spring MVC");
-
+        log.info("usuario que hizo login: " + user);
         model.addAttribute("persons", persons);
-
+        
         return "index";
     }
-
+    
     @GetMapping("/add")
     public String add(Person person) {
         return "modify";
     }
-
+    
     @PostMapping("/save")
     public String save(@Validated Person person, Errors errors) {
         if (errors.hasErrors()) {
@@ -44,14 +46,14 @@ public class StartupController {
         servicePerson.save(person);
         return "redirect:/";
     }
-
+    
     @GetMapping("/edit/{idPerson}")
     public String edit(Person person, Model model) {
         person = servicePerson.findPerson(person);
         model.addAttribute("person", person);
         return "modify";
     }
-
+    
     @GetMapping("/delete")
     public String delete(Person person) {
         servicePerson.delete(person);
