72c83c3d1ecf010754f3cc8fbf1e6bcaff6bbaa2
Leonardo <leonardo38361@gmail.com>
2024-08-01 17:33:30 -0300

    total clientes y balance

--- src/main/java/com/springbloom/SpringBloom/web/StartupController.java
+++ src/main/java/com/springbloom/SpringBloom/web/StartupController.java
@@ -25,40 +25,49 @@ public class StartupController {
     @GetMapping("/")
     public String start(Model model, @AuthenticationPrincipal User user) {
         var persons = servicePerson.listPersons();
-        
-        for(Person person : persons){
+
+        for (Person person : persons) {
             person.setFormattedBalance(FormatUtil.formatCurrency(person.getBalance()));
         }
-        
+
+        double totalBalance = 0D;
+        for (Person person : persons) {
+            if (person.getBalance() != null) {
+                totalBalance += person.getBalance();
+            }
+        }
+
         log.info("Ejecutando el controlador Spring MVC");
         log.info("usuario que hizo login: " + user);
         model.addAttribute("persons", persons);
-        
+        model.addAttribute("formattedTotalBalance", FormatUtil.formatCurrency(totalBalance));
+        model.addAttribute("totalCustomers", persons.size());
+
         return "index";
     }
-    
+
     @GetMapping("/add")
     public String add(Person person) {
         return "modify";
     }
-    
+
     @PostMapping("/save")
     public String save(@Validated Person person, Errors errors) {
         if (errors.hasErrors()) {
             return "modify";
         }
-        
+
         servicePerson.save(person);
         return "redirect:/";
     }
-    
+
     @GetMapping("/edit/{idPerson}")
     public String edit(Person person, Model model) {
         person = servicePerson.findPerson(person);
         model.addAttribute("person", person);
         return "modify";
     }
-    
+
     @GetMapping("/delete")
     public String delete(Person person) {
         servicePerson.delete(person);
